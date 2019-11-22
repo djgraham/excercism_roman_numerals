0fe2c08de0c2cce887095a846ea456d5d217888c
David Graham <david.graham@freeagent.com>
2019-11-22 02:01:27 +0000

    WIP - all tests passing

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 =begin
 Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
 `roman_numerals_test.rb` pass.
@@ -7,22 +9,39 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 module RomanNumerals
-  def to_roman
-    if self > 5
-      "V" + "I" * (self - 5)
-    elsif self > 4
-      "V"
-    elsif self > 3
-      "IV"
-    else
-      "I" * self
-    end
+  NUMERALS = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+     90 => "XC",
+     50 => "L",
+     40 => "XL",
+     10 => "X",
+     9 => "IX",
+     5 => "V",
+     4 => "IV",
+     1 => "I"
+  }
 
+  def to_roman
+    value = self.dup
+    str = ''
+    while value > 0
+      NUMERALS.each do |key, symbol|
+        if value >= key
+          str += symbol
+          value -= key
+          break
+        end
+      end
+    end
+    str
   end
 
 end
 
 class Integer
   include RomanNumerals
-
 end
